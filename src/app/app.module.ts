4c3be526ebeaf1616d7f1f951f2a2e5f58c72647
Jungea <jungea2241@gmail.com>
2022-01-03 17:31:24 +0900

    bootstrap 사용

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Modules
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Components
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [  //Components
+    AppComponent, 
   ],
-  imports: [
+
+  imports: [  //Modules
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule, 
+    ReactiveFormsModule,
   ],
-  providers: [],
+
+  providers: [  //Services
+
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
